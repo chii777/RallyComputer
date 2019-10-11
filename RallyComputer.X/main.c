5880d9d5198fef64b510f1c8cfa74c334c8c071b
calm0815(kuwano) <ku_skyline_wa@yahoo.co.jp>
2019-10-12 00:57:29 +0900

    using MCC plugin & set configuration bits

--- RallyComputer.X/main.c
+++ RallyComputer.X/main.c
@@ -1,62 +1,47 @@
-/* 
- * File:   main.c
- * Author: kuwano
- *
- * Created on 2019/09/25, 23:50
- */
+/**
+  Generated Main Source File
 
-// PIC16F18326 Configuration Bit Settings
+  Company:
+    Microchip Technology Inc.
 
-// 'C' source line config statements
+  File Name:
+    main.c
 
-// CONFIG1
-#pragma config FEXTOSC = OFF    // FEXTOSC External Oscillator mode Selection bits (Oscillator not enabled)
-#pragma config RSTOSC = HFINT32 // Power-up default value for COSC bits (HFINTOSC with 2x PLL (32MHz))
-#pragma config CLKOUTEN = OFF   // Clock Out Enable bit (CLKOUT function is disabled; I/O or oscillator function on OSC2)
-#pragma config CSWEN = ON       // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
-#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enable (Fail-Safe Clock Monitor is enabled)
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
 
-// CONFIG2
-#pragma config MCLRE = ON       // Master Clear Enable bit (MCLR/VPP pin function is MCLR; Weak pull-up enabled)
-#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
-#pragma config WDTE = ON        // Watchdog Timer Enable bits (WDT enabled, SWDTEN is ignored)
-#pragma config LPBOREN = OFF    // Low-power BOR enable bit (ULPBOR disabled)
-#pragma config BOREN = ON       // Brown-out Reset Enable bits (Brown-out Reset enabled, SBOREN bit ignored)
-#pragma config BORV = LOW       // Brown-out Reset Voltage selection bit (Brown-out voltage (Vbor) set to 2.45V)
-#pragma config PPS1WAY = ON     // PPSLOCK bit One-Way Set Enable bit (The PPSLOCK bit can be cleared and set only once; PPS registers remain locked after one clear/set cycle)
-#pragma config STVREN = ON      // Stack Overflow/Underflow Reset Enable bit (Stack Overflow or Underflow will cause a Reset)
-#pragma config DEBUG = OFF      // Debugger enable bit (Background debugger disabled)
-
-// CONFIG3
-#pragma config WRT = OFF        // User NVM self-write protection bits (Write protection off)
-#pragma config LVP = ON         // Low Voltage Programming Enable bit (Low Voltage programming enabled. MCLR/VPP pin function is MCLR. MCLRE configuration bit is ignored.)
-
-// CONFIG4
-#pragma config CP = OFF         // User NVM Program Memory Code Protection bit (User NVM code protection disabled)
-#pragma config CPD = OFF        // Data NVM Memory Code Protection bit (Data NVM code protection disabled)
-
-// #pragma config statements should precede project file includes.
-// Use project enums instead of #define for ON and OFF.
-
-#include <xc.h>
-#include <stdio.h>
-#include <stdlib.h>
-#define _XTAL_FREQ 8000000
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.77
+        Device            :  PIC16F18326
+        Driver Version    :  2.00
+*/
 
 /*
- * 
- */
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
 
-void PICinit()
-{
-    OSCFRQbits.HFFRQ = 0b0011; //8MHz
-    ANSELA  = 0b00000000; // AN?????
-    TRISA   = 0b00000000; // set all of RA pins output
-    TRISC   = 0b00000000; // set all of RC pins output
-    PORTA   = 0b00000000; // All of RA pins set LOW
-    PORTC   = 0b00000000; // All of RC pins set LOW
-    return;
-}
+#include "mcc_generated_files/mcc.h"
 
 void printNumber(int num)
 {
@@ -120,21 +105,34 @@ void setNum(int first, int second)
     RA1=1;
 }
 
-int main(int argc, char** argv) 
+/*
+                         Main application
+ */
+void main(void)
 {
-    PICinit();      //init PIC
-    
-    int i = 0;
-    while(1)
+    // initialize the device
+    SYSTEM_Initialize();
+
+    // When using interrupts, you need to set the Global and Peripheral Interrupt Enable bits
+    // Use the following macros to:
+
+    // Enable the Global Interrupts
+    //INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptEnable();
+
+    // Disable the Global Interrupts
+    //INTERRUPT_GlobalInterruptDisable();
+
+    // Disable the Peripheral Interrupts
+    //INTERRUPT_PeripheralInterruptDisable();
+
+    while (1)
     {
-        for (int k=0; k<200; k++)
-        {
-            setNum(i/10%10, i%10);
-        }
-        i++;
+        // Add your application code
     }
-    setNum(0, 0);    
-
-    return (EXIT_SUCCESS);
 }
-
+/**
+ End of File
+*/
